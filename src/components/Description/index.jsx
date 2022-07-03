3b42c2843877927e024dc06e48d11bbda3739383
AndreyMachula <amcz2020@gmail.com>
2022-07-03 20:09:33 +0200

    Create index.jsx

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,20 @@
+import { PropTypes } from "prop-types";
+import styles from "./styles.module.css";
+
+const Description = ({ username, tag, location, avatar }) => (
+  <div className={styles.description}>
+    <img src={avatar} alt="User avatar" className="avatar" />
+    <p className={styles.name}>{username}</p>
+    <p className={styles.tag}>@{tag}</p>
+    <p className={styles.location}>{location}</p>
+  </div>
+);
+
+Description.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+};
+
+export default Description;
