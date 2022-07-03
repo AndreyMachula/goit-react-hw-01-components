c69ae56768059c057fc5ee56dc17217ce519bca3
AndreyMachula <amcz2020@gmail.com>
2022-07-03 20:09:38 +0200

    Create index.jsx

---
+++ src/components/FriendList/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import styles from "./styles.module.css";
+import FriendListItem from "components/FriendListItem";
+
+const FriendsList = ({ friends }) => {
+  return (
+    <ul className={styles.friend_list}>
+      {friends.map((friend) => (
+        <FriendListItem key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+};
+
+FriendsList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FriendsList;
