946fbe44c999d758861e7e0df45f12c8d45ae8a7
AndreyMachula <amcz2020@gmail.com>
2022-07-03 20:09:42 +0200

    Create index.jsx

---
+++ src/components/FriendListItem/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import styles from "./styles.module.css";
+
+const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+  const color = isOnline ? "green" : "red";
+
+  return (
+    <li className={styles.item}>
+      <span className={styles.status} style={{ "--color": color }}></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }),
+};
+
+export default FriendListItem;
